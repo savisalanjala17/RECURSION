b9947064ef9947b6e19dd9953e6c7f0667635d2f
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-13 19:29:48 +0530

    Update 1. Number of Steps to Reduce a Number to Zero.java

--- LEETCODE/EASY/1. Number of Steps to Reduce a Number to Zero.java
+++ LEETCODE/EASY/1. Number of Steps to Reduce a Number to Zero.java
@@ -1,3 +1,26 @@
+class Solution {
+    public int numberOfSteps(int num) {
+
+        if(num==0){
+            return 0;
+        }
+
+        else if(num%2==0){
+            
+            return 1+ numberOfSteps(num/2);
+        }
+
+        else{
+            
+            return 1+numberOfSteps(num-1);
+        }
+        
+    }
+}
+
+
+
+
 1342. Number of Steps to Reduce a Number to Zero
 
 Given an integer num, return the number of steps to reduce it to zero.
